72e2a07ea20bef9a35e96353b323a0d37115ac46
Assaf <assafoh72@gmail.com>
2023-07-29 13:37:59 +0300

    add a Custom password validator function inside the ts of component sigh-up

--- src/app/components/auth/sign-up/sign-up.component.ts
+++ src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from "@angular/forms";
 import { Router } from '@angular/router';
 
 
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit{
+export class SignUpComponent implements OnInit {
 
 
   form!: FormGroup;
@@ -18,12 +18,28 @@ export class SignUpComponent implements OnInit{
     this.form = new FormGroup ({
       name: new FormControl ('', Validators.required ),
       email: new FormControl ('', [Validators.required, Validators.email] ),
-      password: new FormControl ('', Validators.required)
+      password: new FormControl ('', [Validators.required, this.passwordValidator()])  // Add the custom validator to the password control
     });
   }
   handleSubmit(): void {
     console.log(this.form)
     this.router.navigate(['/app-heroes-list']);
   }
+
+
+  // Custom password validator function
+  passwordValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      const password = control.value;
+
+      // Check if the password meets the requirements
+      if (password && /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(password)) {
+        return null; // Password is valid, return null (no errors)
+      } else {
+        return { invalidPassword: true }; // Password is invalid, return an error object
+      }
+    };
+  }
+
 
 }
